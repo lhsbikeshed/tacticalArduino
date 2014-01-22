784d762486410f7f5338b427a98ec2a24286ebd9
top <tom@imakethin.gs>
2014-01-22 20:18:49 +0000

    tidied code, added comments, and changed the cable puzzle to just output connects and disconnects

--- tacticalArduino.ino
+++ tacticalArduino.ino
@@ -1,5 +1,26 @@
 #include <avr/pgmspace.h>
+/*
+ * Controls tactical console
+ * outputs:
+ * ' ' for scan button
+ * 0-9 for enumpad
+ * f for beam with no charge
+ * F for beam that is ready
+ * m for decoy
+ * cX for disconnected cable X
+ * CX for connected cable X
+ *
+ * inputs:
+ * d = damage flicker
+ * T for flap open
+ * S for smoke machine
+ * sX for smoke machine blast of X * 500 ms (x = 0-9)
+ * p for power off
+ * P for power on
+ * C dump out the current cable state (in cX/CX format above)
+ */
 
+//keymap
 PROGMEM prog_uchar charMap[] = {  
   'g', '#', 'f', 'e', 'd', 'c', 'b', 'a', 
   't', 's', 'r', 'q', 'p', 'o', 'n', 'm',
@@ -16,51 +37,69 @@ orn     common led anode (i.e. +5V)
  wht     1a              led strobe              
  yel     5y              from buttons data (output from the board)
  */
-
+//keyboard pin defs
 #define CLOCKPIN 2   //BLUE
 #define DATAPIN  3     //YELLOW
 #define READPIN  4    //PURPLE
 #define LEDSTROBE 5   //WHITE
 #define LEDDATA  6   //GREEN
 
+//game state defs
+#define STATE_START 0
+#define STATE_PLAY 1
+#define STATE_COMPLETE 2
+
+//other pin defs
+#define SMOKEPIN 19
+#define TACPANELPIN 12
+#define TUBEPIN 7
+
+
+//last state of buttons from keyboard
 long lastButtonState = 0;
+//led states
 long ledState = 0;
 
+//is the console powered on?
 boolean poweredOn = false;
 int poweredOnTimer = 320;
+//time taken to charge teh layzors
 int chargeTimer = 0;
 
+//charge levels of each bank
 byte beamCharge[] = {
   0,0,0,0};
+
+//toggles on and off every second, for animation purposes
 boolean blinker = false;
 
+//is decoy light blinking?
 boolean decoyBlink = false;
+//should the large wall tube blink?
 boolean tubeBlink = false;
 long tubeBlinkTimer = 0;
+
+//rate at which we recharge the layzors
 int chargeRate = 5;
+
+
+
+
+//last state of the cable puzzle
 long lastPanelRead = 0;
+byte debounceCableState = 0;  //for 30ms debounce things
+byte lastCableState = 0;      //the last confirmed read of the cables state
 
-//conduit things
-
-
-byte lastCableState = 0;
-byte cableState = 0;
 
 char buffer[10]; //serial buffer
 byte bufPtr = 0;
 
 byte connectionOrder[] = {  
   2,0,1,4,3 };
-//'y, s, b, p, w*/
+//white, yellow, black, blue, red*/
 byte currentConnection = 0;
 
-#define STATE_START 0
-#define STATE_PLAY 1
-#define STATE_COMPLETE 2
 
-#define SMOKEPIN 19
-#define TACPANELPIN 12
-#define TUBEPIN 7
 
 byte state = STATE_START;
 boolean first = true;
@@ -73,6 +112,7 @@ long tacticalPanelTimer = 0;
 boolean tacticalPanelSolenoid = false;
 
 
+//get the current key status and set the LEDS at the same time
 unsigned long boardStatus(unsigned long leds){
   unsigned long buttons = 0;
   byte bbit;
@@ -116,7 +156,7 @@ void processBuffer(){
     poweredOnTimer = 320;
     digitalWrite(TUBEPIN, LOW);
   } 
-  else if (buffer[0] == 'p'){
+  else if (buffer[0] == 'p'){  //power off
     poweredOn = false;
     tubeBlink = false;
     decoyBlink = false;
@@ -126,26 +166,26 @@ void processBuffer(){
       beamCharge[b] = 0;
     }
   }  
-  else if(buffer[0] == 'd'){
+  else if(buffer[0] == 'd'){    //turn off decoy blink effect
     decoyBlink = false;
   } 
-  else if (buffer[0] == 'D'){
+  else if (buffer[0] == 'D'){  //turn on decoy blink effect
     decoyBlink = true;
   } 
-  else if (buffer[0] == 'S'){
+  else if (buffer[0] == 'S'){  //fire some smoke, add 1500ms to the smoke counter
     smoke = true;
     smokeTimer = millis();
 
     smokeDuration += 1500;
     smokeDuration > 5000 ? 5000 : smokeDuration;
   } 
-  else if (buffer[0] == 's'){
+  else if (buffer[0] == 's'){  //add 0-9 * 500ms of smoke to counter
     smoke = true;
     smokeTimer = millis();
     if( buffer[1] >= '1' && buffer[1] <= '9'){
       int dur = (buffer[1] - 48) * 500;
       smokeDuration += 1500;
-    //Serial.println(dur);
+      //Serial.println(dur);
     } 
     else {
       smokeDuration += 1500;
@@ -165,14 +205,26 @@ void processBuffer(){
       break;
     }
   } 
-  else if (buffer[0] == 'T'){
+  else if (buffer[0] == 'T'){    //trigger the drop flap
     tacticalPanelSolenoid = true;
     tacticalPanelTimer = millis();
   } 
-  else if (buffer[0] == 'Q'){
+  else if (buffer[0] == 'Q'){    //blink the large wall tube (not currently used)
     if(poweredOn){
       tubeBlink = true;
       tubeBlinkTimer = millis();
+    }
+  } 
+  else if (buffer[0] == 'C'){    //dump out the current cable state
+    for(int i = 0; i < 5; i++){
+      if( digitalRead(14 + i)){
+        Serial.print("c");
+      } 
+      else {
+        Serial.print("C");
+      }
+      Serial.print(i);
+      Serial.print(",");
     }
   }
 
@@ -199,6 +251,7 @@ void setup()
   ledState = 1;
   long btn = boardStatus(ledState);
 
+  //setup the pins for the cable puzzle, inputs with pullups
   for(int i = 0; i < 5; i++){
     pinMode(14 + i, INPUT);
     digitalWrite(14 + i, HIGH);
@@ -213,25 +266,51 @@ void setup()
     pinMode(i, OUTPUT);
   }
 
+  //smoke stuff
   pinMode(SMOKEPIN, OUTPUT);
   digitalWrite(SMOKEPIN,LOW);
   lastPanelRead = millis();
   pinMode(TACPANELPIN, OUTPUT);
   digitalWrite(TACPANELPIN, LOW);
-}
-void fail(){
-  currentConnection = 0;
-  lastCableState = 0;
-  Serial.print("X,");
-  state = STATE_START;
+
+  //read the first set of cable states
+  for(int i = 0; i < 5; i++){
+    lastCableState |= (digitalRead(14 + i) << i);
+  }  
 }
 
-unsigned long ct = 0;
+void processCableState(byte in){
+  if(in == lastCableState){
+    return;
+  }
+  //now work out what changed
+  for(int i = 0; i < 5; i++){
+    if( (in  & (1 << i) )!= (lastCableState &  (1 << i))){
+      if(in & ( 1 << i)){
+        Serial.print("c");
+      } 
+      else {
+        Serial.print("C");
+      } 
+      Serial.print(i);
+      Serial.print(",");
+    }        
+  } 
+
+
+
+
+  lastCableState = in;
+}
+
+
 void loop()
 {
+  long currentTime = millis();
 
-  //tactical panel
-  if(tacticalPanelSolenoid && tacticalPanelTimer + 500 > millis()){
+
+  //--------------- tactical panel
+  if(tacticalPanelSolenoid && tacticalPanelTimer + 500 > currentTime){
     digitalWrite(TACPANELPIN, HIGH);
   } 
   else {
@@ -239,81 +318,24 @@ void loop()
     digitalWrite(TACPANELPIN, LOW);
   }
 
-  //conduit
-  if(state == STATE_START){
-    byte c = 0;
+  //----------------- conduit puzzle
+  if(lastPanelRead + 30 < currentTime){
+    //debounce stuff
+    lastPanelRead = currentTime;
+    byte b = 0;
     for(int i = 0; i < 5; i++){
-      c |= (1 - digitalRead(14 + i)) << i;
+      b |= (digitalRead(14 + i) << i);
     }
+    //if its settled then go process it
+    if (b == debounceCableState){
 
-    if(c == 0){
-      state = STATE_PLAY;
-      if(!first){
-        Serial.print("R,");
-      } 
-      else {
-        first = false;
-      }
-    } 
-  } 
-  else if(state == STATE_PLAY){
-    int curCable = connectionOrder[currentConnection];
-
-    byte c = 0;    
-    for(int i= 0; i < 5; i++){
-
-      byte a = digitalRead(14 + i);
-      //delay(10);
-
-
-      if(a == LOW && a == digitalRead(14 + i)){
-
-        c |= 1 << i;
-      }
+      processCableState(b);
     }
-    byte changes = c ^ lastCableState;
-    // Serial.println(changes,BIN);
-    if(changes != 0){  //it changed!
+    debounceCableState = b;
 
-      for(int i = 0; i < 5; i++){ //find out what and how
-        if( changes & 1 << i )  //this bit changed
-          if(c & 1 << i)  {  //this loops cable state changed to connected
-            if( curCable == i ){
-              Serial.print("C");
-              Serial.print(currentConnection);
-              Serial.print(",");              
-              currentConnection += 1;
-              if(currentConnection > 4){
-                state = STATE_COMPLETE;
-                Serial.print("P,");
-              }
-            } 
-            else {
-              fail();
-            }
-          } 
-      }
-      lastCableState = c;
-    }
-
-
-
-
-
-  } 
-  else {
-    byte c = 0;
-    for(int i = 0; i < 5; i++){
-      c |= (1 - digitalRead(14 + i)) << i;
-    }
-
-    if(c == 0){
-      state = STATE_START;
-      Serial.print("R,");
-    } 
   }
 
-  //serial reads --------------
+  //-------------------- serial reads --------------
   while ( Serial.available() > 0) {  // If data is available,
     char c = Serial.read();
     if(c == ','){
@@ -332,16 +354,17 @@ void loop()
     }
 
   }
-
+  //-------------------- smoke machine
   if(smoke == true){
     digitalWrite(SMOKEPIN, HIGH);
-    if(smokeTimer + smokeDuration < millis()){
+    if(smokeTimer + smokeDuration < currentTime){
       smoke = false;
       smokeDuration = 0;
       digitalWrite(SMOKEPIN, LOW);
     }
   }
 
+  //------------ panel power, if off then nothing past here executes
   if(poweredOn == false) {
     ledState = 0;
     long btn = boardStatus(ledState);
@@ -351,7 +374,7 @@ void loop()
 
   //reset leds
   ledState = 0;
-
+  //chargin teh layzors
   chargeTimer ++;
   if(chargeTimer > 20){
     chargeTimer = 0;
@@ -365,6 +388,7 @@ void loop()
       }
     }
   }
+  //update laser leds
   for (int b = 0; b < 4; b++){
 
     int c = beamCharge[b] / 50;
@@ -392,12 +416,12 @@ void loop()
 
     ledState |= (long)v << (b * 4);
   } 
-
+  //and the decoy blinker
   if (decoyBlink && blinker){
     ledState |= 1l <<  18;
     ledState |= 1l <<  19;
   }
-
+  //read the buttons and push leds
   long btn = boardStatus(ledState);
   //Serial.println(btn,BIN);
   if(btn != lastButtonState){
@@ -443,6 +467,10 @@ void loop()
 
   delay(10);
 }
+
+
+
+
 
 
 
